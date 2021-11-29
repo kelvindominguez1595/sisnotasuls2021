4c3ff879a592aea81fd7c6d8bca1a99429a2e099
Kelvin Dominguez <kelvin.dominguez95@gmail.com>
2021-11-28 18:55:44 -0600

    fixed notas

---
+++ Controller/NotasController.php
@@ -0,0 +1,149 @@
+<?php
+// importamos nuestro modelo
+require_once 'Model/Alumnos.php';
+require_once 'Model/Grados.php';
+require_once 'Model/Secciones.php';
+require_once 'Model/Usuarios.php';
+require_once 'Model/Direcciones.php';
+
+class NotasController{
+    // para accender al modelo y sus atributos
+    private $model;
+    private $modelgrado;
+    private $modelseccion;
+    private $modeluser;
+    private $diremodel;
+
+    // Constructos
+    public function __CONSTRUCT(){
+        $this->model = new Alumnos();
+        $this->modelseccion = new Secciones();
+        $this->modelgrado = new Grados();
+        $this->modeluser = new Usuarios();
+        $this->diremodel = new Direcciones();
+    }
+
+   /** Inicio de llamado de la vistas */
+    public function Index(){
+        require_once 'views/backend/header.php';
+        require_once 'views/backend/notas/index.php';
+        require_once 'views/backend/footer.php';
+    }
+    public function Nuevo(){
+        $id = $_REQUEST['id'];
+        $zona = $_REQUEST['zona'];
+        require_once 'views/backend/header.php';
+        require_once 'views/backend/notas/crear.php';
+        require_once 'views/backend/footer.php';
+    }
+
+    public function Editar(){
+        // Capturamos el id enviado por get
+        $id = $_REQUEST['id'];
+        // crear el metodo para listar un dato especifico
+        $data = $this->model->obtenerRegistro($id);
+        require_once 'views/backend/header.php';
+        require_once 'views/backend/notas/editar.php';
+        require_once 'views/backend/footer.php';
+    }
+    public function Borrar(){
+        // Capturamos el id enviado por get
+        $id = $_REQUEST['id'];
+        require_once 'views/backend/header.php';
+        require_once 'views/backend/notas/borrar.php';
+        require_once 'views/backend/footer.php';
+    }
+    /** Fin de llamado de la vistas */
+
+    /** Metodos CRUD */    
+    public function Registrar(){
+        // capturo los valores enviados por post o get
+        $this->model->nombres = $_REQUEST['nombres'];
+        $this->model->apellidos = $_REQUEST['apellidos'];
+        $this->model->genero = $_REQUEST['genero'];
+        $this->model->fechanac = $_REQUEST['fechanac'];
+        $this->model->matriculaid = $_REQUEST['matriculaid'];
+        $this->model->seccionid = $_REQUEST['seccionid'];
+        $this->model->gradoid = $_REQUEST['gradoid'];
+   
+
+        $this->modeluser->usuario     = $_REQUEST['nombres'];
+        $this->modeluser->email       = $_REQUEST['email'];
+        $this->modeluser->pass        = $_REQUEST['pass'];
+ 
+
+         // utilizamos el metodo de guardar de SQL
+         $res = $this->model->Registrar($this->model);
+        if($res){
+            $this->modeluser->alumnoid  = $res;
+            $resuuser = $this->modeluser->RegistrarAlumno($this->modeluser);
+            if($resuuser){
+           
+
+                $this->diremodel->direccion = "-";
+                $this->diremodel->telefono = "-";
+                $this->diremodel->usuarioid = $resuuser;
+                $this->diremodel->zona = $_REQUEST['zona'];
+                if($this->diremodel->Registrar($this->diremodel)){
+                    $texto = "Registro exitosamente";
+                    $tipo = "success";
+                    $this->model->SesionesMessage($texto, $tipo);
+                } else{
+                    $texto = "Ocurrio un error";
+                    $tipo = "error";
+                    $this->model->SesionesMessage($texto, $tipo);
+                }
+            }else {
+                $texto = "Ocurrio un error";
+                $tipo = "error";
+                $this->model->SesionesMessage($texto, $tipo);
+            }
+        }else{
+            $texto = "Ocurrio un error";
+            $tipo = "error";
+            $this->model->SesionesMessage($texto, $tipo);
+        }
+    }
+
+    public function Actualizar(){
+        // capturo los valores enviados por post o get
+        $this->model->id = $_REQUEST['id'];
+        $this->model->nombres = $_REQUEST['nombres'];
+        $this->model->apellidos = $_REQUEST['apellidos'];
+        $this->model->genero = $_REQUEST['genero'];
+        $this->model->fechanac = $_REQUEST['fechanac'];
+
+        $this->model->seccionid = $_REQUEST['seccionid'];
+        $this->model->gradoid = $_REQUEST['gradoid'];
+   
+
+        // utilizamos el metodo de guardar de SQL
+        if($this->model->actualizar($this->model)){
+            $texto = "Actualizó exitosamente";
+            $tipo = "success";
+            $this->model->SesionesMessage($texto, $tipo);
+        }else{
+            $texto = "Ocurrio un error";
+            $tipo = "error";
+            $this->model->SesionesMessage($texto, $tipo);
+        }
+    }
+
+    public function BorrarId(){
+        // capturo los valores enviados por post o get
+        $this->model->id = $_REQUEST['id'];
+        // utilizamos el metodo de guardar de SQL
+        if($this->model->delete($this->model)){            
+            $texto = "Registro borrado exitosamente";
+            $tipo = "success";
+            $this->model->SesionesMessage($texto, $tipo);
+        }else{
+            $texto = "Ocurrio un error ";
+            $tipo = "error";
+            $this->model->SesionesMessage($texto, $tipo);
+        }
+    }
+
+
+}
+?>
---
+++ views/backend/notas/index.php
@@ -0,0 +1,39 @@
+  <!-- Begin Page Content -->
+  <div class="container-fluid">
+    <!-- Page Heading -->
+    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+      <h1 class="h3 mb-0 text-gray-800">Calificar</h1>
+      <?php if(isset($_SESSION['texto'])){?>
+            <div class="alert alert-<?php if($_SESSION['tipo'] == "success"){ echo "success";}else{echo "danger"; }?> alert-dismissible fade show" role="alert">
+            <strong>                   
+                    <?php if($_SESSION['tipo'] == "success"){ echo "Exitos!!! 😊";}else{echo "Ooops! Ah Ocurrido un error 😱"; }?>
+                 </strong> 
+                <?php echo $_SESSION['texto'];?>
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+                <?php unset($_SESSION["texto"]); unset($_SESSION["tipo"]); ?>
+                </button>
+            </div>
+      <?php } ?>
+      
+    </div>
+
+    <div class="col-xl-3 col-md-6 mb-4">
+                <div class="card border-left-primary shadow h-100 py-2">
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                            <div class="col mr-2">
+                                <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                    Earnings (Monthly)</div>
+                                <div class="h5 mb-0 font-weight-bold text-gray-800">$40,000</div>
+                            </div>
+                            <div class="col-auto">
+                                <i class="fas fa-calendar fa-2x text-gray-300"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                </div>
+
+  </div>
+        <!-- /.container-fluid -->
